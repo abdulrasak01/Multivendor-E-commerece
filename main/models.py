42f1f1fa3a2f6f6d94c9212124e809af460a4f92
abdulrasak <abdulrasakn2004@gmail.com>
2024-08-06 17:03:36 +0530

    Learn Django

---
+++ main/models.py
@@ -0,0 +1,26 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+# Vendor models
+class Vendor(models.Model):
+    user = models.ForeignKey(User,on_delete=models.CASCADE)
+    address = models.TextField(null = True)
+    
+#Product category
+class productCategory(models.Model):
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    
+    def __str__(self):
+        return self.title
+    
+#Product
+class Product:
+    title = models.CharField(max_length=200)
+    detail = models.TextField(null=True)
+    price = models.FloatField()
+    
+    def __str__(self):
+        return self.title
+    
+    
